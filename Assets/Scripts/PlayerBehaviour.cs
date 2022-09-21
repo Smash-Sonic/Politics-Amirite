38d7af1bdafd56ed02d2af14ef6ffcccbfb2bc6b
JeremiahPeters <jcpeters@mail.bradley.edu>
2022-09-21 10:49:03 -0500

    picking up stuff
    player can left click to "pick up" the person object and move them around, then click to put them back down but there's an issue so you have to spam click to get it to work rn.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -5,16 +5,51 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     //mouse location = cam.ScreenToWorldPoint(Input.mousePosition
+    public GameObject PickedUp;
 
-    // Start is called before the first frame update
+    public Camera cam;
+    public Coroutine HoldingObject;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+        transform.position = new Vector3(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y, 0);
         
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D hitCollider = Physics2D.OverlapBox(gameObject.transform.position, transform.localScale / 2, 0f);
+            if (hitCollider != null)
+            {
+                if (hitCollider.name == "Person")
+                {
+                    HoldingObject = StartCoroutine(PickUp(hitCollider));
+                }
+                else if (hitCollider.name == "EndButton") 
+                {
+                    Debug.Log("end phase?");
+                }
+                //Debug.Log(hitCollider.name);
+            }
+        }
+    }
+
+    IEnumerator PickUp(Collider2D PickedUp) 
+    {
+        yield return new WaitForSeconds(0.01f);
+        while (true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                StopCoroutine(HoldingObject);
+                break;
+            }
+            yield return new WaitForSeconds(0.01f);
+            PickedUp.transform.position = transform.position;
+        }
+        yield return null;
     }
 }
