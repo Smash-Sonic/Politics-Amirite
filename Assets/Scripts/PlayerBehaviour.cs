d64b284356ab17b3175d0ebf8c94e40b97227a75
JeremiahPeters <jcpeters@mail.bradley.edu>
2022-09-24 18:20:35 -0500

    nearly finished phase 1 (the hard part)
    added ability for employees to be delegated to tasks, and the end phase button checks if all have been delegated, thus phase 1 has been roughly completed
    next step is the other 2 phases which shouldn't be hard

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    //mouse location = cam.ScreenToWorldPoint(Input.mousePosition
-    public GameObject PickedUp;
-
     public Camera cam;
     public Coroutine HoldingObject;
+    public Collider2D[] HitColliders;
+    public GameObject[] EmployeeList;
+    public int EmployeesOwned = 0;
+    public int EmployeesDelegated = 0;
+    public bool IsHolding = false;
 
     void Start()
     {
-
+        EmployeeList = GameObject.FindGameObjectsWithTag("Person");
+        for (int i = 0; i < EmployeeList.Length; i++)
+        {
+            Debug.Log(EmployeeList[i]);
+        }
+        EmployeesOwned = (EmployeeList.Length);
+        Debug.Log(EmployeesOwned);
     }
 
     void Update()
@@ -21,18 +29,58 @@ public class PlayerBehaviour : MonoBehaviour
         
         if (Input.GetMouseButtonDown(0))
         {
-            Collider2D hitCollider = Physics2D.OverlapBox(gameObject.transform.position, transform.localScale / 2, 0f);
-            if (hitCollider != null)
+            //check for object to pick up or button to press
+            HitColliders = null;
+            HitColliders = Physics2D.OverlapBoxAll(gameObject.transform.position, transform.localScale / 2, 0f);
+
+            for (int i = 0; i < HitColliders.Length; i++)
             {
-                if (hitCollider.name == "Person")
+                Debug.Log(HitColliders[i]);
+            }
+
+            if (IsHolding == false)
+            {
+                if (HitColliders[0] != null)
                 {
-                    HoldingObject = StartCoroutine(PickUp(hitCollider));
+                    if (HitColliders[0].tag == "Person")
+                    {
+                        IsHolding = true;
+                        for (int i = 0; i < HitColliders.Length; i++)
+                        {
+                            if (HitColliders[i].tag == "Target")
+                            {
+                                EmployeesDelegated--;
+                            }
+                        }
+                        HoldingObject = StartCoroutine(PickUp(HitColliders[0]));
+                    }
+                    else if (HitColliders[0].name == "EndButton")
+                    {
+                        //heck if all employees have been delegated
+                        if (EmployeesDelegated >= EmployeesOwned)
+                        {
+                            Debug.Log("end phase?");
+                        }
+                        else
+                        {
+                            Debug.Log("Please Delegate all employees to a task.");
+                        }
+                    }
+                    //Debug.Log(hitCollider.name);
                 }
-                else if (hitCollider.name == "EndButton") 
+            }
+            else if (IsHolding == true)
+            {
+                //let go of held object
+                for (int i = 0; i < HitColliders.Length; i++)
                 {
-                    Debug.Log("end phase?");
+                    if (HitColliders[i].tag == "Target")
+                    {
+                        EmployeesDelegated++;
+                    }
                 }
-                //Debug.Log(hitCollider.name);
+                IsHolding = false;
+                StopCoroutine(HoldingObject);
             }
         }
     }
@@ -42,14 +90,8 @@ public class PlayerBehaviour : MonoBehaviour
         yield return new WaitForSeconds(0.01f);
         while (true)
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                StopCoroutine(HoldingObject);
-                break;
-            }
             yield return new WaitForSeconds(0.01f);
             PickedUp.transform.position = transform.position;
         }
-        yield return null;
     }
 }
