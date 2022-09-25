6ca11a13982305dbbfe039cf7d487a7ed4c8e0ec
JeremiahPeters <jcpeters@mail.bradley.edu>
2022-09-25 13:39:34 -0500

    phase 2 working basically
    yea

--- Assets/Scripts/Phase2PlayerBehaviour.cs
+++ Assets/Scripts/Phase2PlayerBehaviour.cs
@@ -2,43 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Phase1PlayerBehaviour : MonoBehaviour
+public class Phase2PlayerBehaviour : MonoBehaviour
 {
     public Camera cam;
     public Coroutine HoldingObject;
     public Collider2D[] HitColliders;
-    public GameObject[] EmployeeList;
-    public int EmployeesOwned = 0;
-    public int EmployeesDelegated = 0;
     public bool IsHolding = false;
-
-    void Start()
-    {
-        EmployeeList = GameObject.FindGameObjectsWithTag("Person");
-        for (int i = 0; i < EmployeeList.Length; i++)
-        {
-            Debug.Log(EmployeeList[i]);
-        }
-        EmployeesOwned = (EmployeeList.Length);
-        Debug.Log(EmployeesOwned);
-    }
+    public bool Delegated = false;
 
     void Update()
     {
         transform.position = new Vector3(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y, 0);
-        
+
         if (Input.GetMouseButtonDown(0))
         {
             //check for object to pick up or button to press
             HitColliders = null;
             HitColliders = Physics2D.OverlapBoxAll(gameObject.transform.position, transform.localScale / 2, 0f);
-
-            //put all the touched colliders in debug log
+            /*
             for (int i = 0; i < HitColliders.Length; i++)
             {
                 Debug.Log(HitColliders[i]);
             }
-
+            */
             if (IsHolding == false)
             {
                 if (HitColliders.Length >= 1)
@@ -46,26 +32,25 @@ public class Phase1PlayerBehaviour : MonoBehaviour
                     if (HitColliders[0].tag == "Person")
                     {
                         IsHolding = true;
-                        //for being picked up off of a target
                         for (int i = 0; i < HitColliders.Length; i++)
                         {
                             if (HitColliders[i].tag == "Target")
                             {
-                                EmployeesDelegated--;
+                                Delegated = false;
                             }
                         }
                         HoldingObject = StartCoroutine(PickUp(HitColliders[0]));
                     }
                     else if (HitColliders[0].name == "EndButton")
                     {
-                        //check if all employees have been delegated
-                        if (EmployeesDelegated >= EmployeesOwned)
+                        //heck if all employees have been delegated
+                        if (Delegated == true)
                         {
-                            Debug.Log("end phase?");
+                            Debug.Log("End phase?");
                         }
                         else
                         {
-                            Debug.Log("Please Delegate all employees to a task.");
+                            Debug.Log("Please Delegate an employees to this task.");
                         }
                     }
                     //Debug.Log(hitCollider.name);
@@ -74,13 +59,11 @@ public class Phase1PlayerBehaviour : MonoBehaviour
             else if (IsHolding == true)
             {
                 //let go of held object
-
-                //for being dropped onto a target
                 for (int i = 0; i < HitColliders.Length; i++)
                 {
                     if (HitColliders[i].tag == "Target")
                     {
-                        EmployeesDelegated++;
+                        Delegated = true;
                     }
                 }
                 IsHolding = false;
@@ -89,7 +72,7 @@ public class Phase1PlayerBehaviour : MonoBehaviour
         }
     }
 
-    IEnumerator PickUp(Collider2D PickedUp) 
+    IEnumerator PickUp(Collider2D PickedUp)
     {
         yield return new WaitForSeconds(0.01f);
         while (true)
