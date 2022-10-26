80ab550eda111e8cc1810150efd098957ee52508
JeremiahPeters <jcpeters@mail.bradley.edu>
2022-10-26 10:45:25 -0500

    textboxes

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -8,10 +8,23 @@ public class CharacterStats : MonoBehaviour
     public int Strength;
     public int Intelligence;
     public int SocialSkills;
+    public GameObject StatBox;
     public TMP_Text StatText;
 
     void Update()
     {
         StatText.text = "Str = " + Strength + " Int = " + Intelligence + " Soc = " + SocialSkills;
+    }
+
+    void OnMouseEnter()
+    {
+
+        Debug.Log("Mouse is over GameObject.");
+    }
+
+    void OnMouseExit()
+    {
+        
+        Debug.Log("Mouse is no longer on GameObject.");
     }
 }
